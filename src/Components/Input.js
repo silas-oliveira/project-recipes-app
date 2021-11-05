838748947783c67a8dbb0997a00f1595d3312ea6
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-11-05 19:19:18 -0300

    criando os inputs e botao e o contextAPI

---
+++ src/Components/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+function Input(props) {
+  const { type, nome, dataTestid } = props;
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin({
+      [name]: value,
+    });
+  };
+
+  return (
+    <label htmlFor={ nome }>
+      <input
+        type={ type }
+        name={ nome }
+        value={ nome }
+        id={ nome }
+        data-testid={ dataTestid }
+        onChange={ handleChange }
+      />
+    </label>
+  );
+}
+
+Input.propTypes = {
+  nome: PropType.string.isRequired,
+  type: PropType.string.isRequired,
+  dataTestid: PropType.string.isRequired,
+};
+
+export default Input;
