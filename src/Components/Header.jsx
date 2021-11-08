bc3171a519dd19f659d66c157fcec2b673860856
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-11-08 14:45:58 -0300

    criandoos elementos do header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <span data-testid="profile-top-btn">Profile</span>
+      <span data-testid="page-title">Title</span>
+      <span data-testid="search-top-btn">Search</span>
+    </div>
+  );
+}
+
+export default Header;
