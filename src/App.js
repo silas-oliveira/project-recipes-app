8b06b47eb0bbe078da272ddb38919ce5b13ef2c3
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-11-08 15:59:52 -0300

    ajustes no footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import ProvideAppReceita from './ContextAPI/ProvideAppReceita';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/Login';
+import Bebidas from './Pages/Bebidas';
+import Comidas from './Pages/Comidas';
 
 function App() {
   return (
     <ProvideAppReceita>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/bebidas" component={ Bebidas } />
+        <Route path="/comidas" component={ Comidas } />
       </Switch>
     </ProvideAppReceita>
   );
