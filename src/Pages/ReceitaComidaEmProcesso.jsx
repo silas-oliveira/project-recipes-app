dc5f93484bb5e8eb083a1e28042e8c5a66069e13
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-11-09 15:52:52 -0300

    requisito 47 em processo

---
+++ src/Pages/ReceitaComidaEmProcesso.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function ReceitaComidaEmProcesso() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" data-testid="recipe-photo" />
+        <div>
+          <h2 data-testid="recipe-title">Titulo</h2>
+          <button type="button" data-testid="share-btn">Compartilhar</button>
+          <button type="button" data-testid="favorite-btn">Favoritar</button>
+        </div>
+        <div>
+          <p data-testid="recipe-category">texto categoria</p>
+          <p data-testid={ `${index}-ingredient-step` }>ingredientes</p>
+        </div>
+        <div>
+          <p data-testid="instructions">Intrução</p>
+        </div>
+        <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+      </div>
+    </div>
+  );
+}
+
+export default ReceitaComidaEmProcesso;
