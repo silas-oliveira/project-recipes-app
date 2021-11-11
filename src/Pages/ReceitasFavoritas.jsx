929111cf66120470a12dc44caad5746ec8e51f60
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-11-11 19:33:17 -0300

    colocando uma classe

--- src/Pages/ReceitasFavoritas.jsx
+++ src/Pages/ReceitasFavoritas.jsx
@@ -15,9 +15,27 @@ function ReceitasFavoritas() {
     <div>
       <Header title="Receitas Favoritas" />
       <h1>Explorar ids</h1>
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-food-btn">Food</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drink</button>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        className="btn btn-outline-primary m-1"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        className="btn btn-outline-primary m-1"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        className="btn btn-outline-primary m-1"
+      >
+        Drink
+      </button>
       <div>
         { favorites !== null && favorites.map((favorite, index) => (
           <div key={ index }>
