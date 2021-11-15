a791276ed161db53b5bc3b8790acdfc676fb20b2
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-11-15 18:37:58 -0300

    Add exercise 65

--- src/Pages/ReceitasFavoritas.jsx
+++ src/Pages/ReceitasFavoritas.jsx
@@ -12,6 +12,21 @@ function ReceitasFavoritas() {
     setFavorites(getFavorites());
   }, []);
 
+  const handleClick = (e) => {
+    const { value } = e.target;
+    switch (value) {
+    case 'Food':
+      setFavorites(favorites.filter((fav) => fav.type === 'comida'));
+      break;
+    case 'Drink':
+      setFavorites(getFavorites().filter((fav) => fav.type === 'bebida'));
+      break;
+    default:
+      setFavorites(getFavorites());
+      break;
+    }
+  };
+
   return (
     <div>
       <Header title="Receitas Favoritas" />
@@ -20,6 +35,8 @@ function ReceitasFavoritas() {
         type="button"
         data-testid="filter-by-all-btn"
         className="btn btn-outline-primary m-1"
+        value="All"
+        onClick={ handleClick }
       >
         All
       </button>
@@ -27,6 +44,8 @@ function ReceitasFavoritas() {
         type="button"
         data-testid="filter-by-food-btn"
         className="btn btn-outline-primary m-1"
+        value="Food"
+        onClick={ handleClick }
       >
         Food
       </button>
@@ -34,6 +53,8 @@ function ReceitasFavoritas() {
         type="button"
         data-testid="filter-by-drink-btn"
         className="btn btn-outline-primary m-1"
+        value="Drink"
+        onClick={ handleClick }
       >
         Drink
       </button>
