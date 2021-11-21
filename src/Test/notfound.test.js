9480870888e11a46b66b8687b09cdd17d69bc293
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-11-21 18:22:09 -0300

    Fix notFound test

--- src/Test/notfound.test.js
+++ src/Test/notfound.test.js
@@ -7,9 +7,9 @@ describe('Verify "not found" component', () => {
   it('Verify if there is H1 with not found on it', () => {
     renderWithRouter(<NotFound />);
 
-    const h1NotFound = screen.getByRole('heading', {
+    const h1NotFound = screen.getAllByRole('heading', {
       level: 1,
-    });
+    })[0];
     expect(h1NotFound).toBeInTheDocument();
   });
 });
