57aef13707d0822690486ce4462bc86fceec2bbd
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-11-23 19:44:19 -0300

    Add search test

--- src/Test/search.test.js
+++ src/Test/search.test.js
@@ -6,6 +6,9 @@ import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
+const SEARCH_INPUT = 'search-input';
+const BUTTON = 'exec-search-btn';
+
 const radioButtons = [
   'ingredient-search-radio', 'name-search-radio', 'first-letter-search-radio',
 ];
@@ -14,8 +17,8 @@ const renderCorrectForms = () => {
   radioButtons.forEach((radioButton) => {
     expect(screen.queryByTestId(radioButton)).not.toBeInTheDocument();
   });
-  expect(screen.queryByTestId('search-input')).not.toBeInTheDocument();
-  expect(screen.queryByTestId('exec-search-btn')).not.toBeInTheDocument();
+  expect(screen.queryByTestId(SEARCH_INPUT)).not.toBeInTheDocument();
+  expect(screen.queryByTestId(BUTTON)).not.toBeInTheDocument();
 
   const searchInput = screen.getByTestId('search-top-btn');
   fireEvent.click(searchInput);
@@ -23,20 +26,24 @@ const renderCorrectForms = () => {
   radioButtons.forEach((radioButton) => {
     expect(screen.getByTestId(radioButton)).toBeInTheDocument();
   });
-  expect(screen.queryByTestId('search-input')).toBeInTheDocument();
-  expect(screen.queryByTestId('exec-search-btn')).toBeInTheDocument();
+  expect(screen.queryByTestId(SEARCH_INPUT)).toBeInTheDocument();
+  expect(screen.queryByTestId(BUTTON)).toBeInTheDocument();
 };
 
-const search = (query, index, endURL) => {
+const search = (query, index, endURL, type) => {
   const searchInput = screen.getByTestId('search-top-btn');
+  const url = type === 'comida' ? 'https://www.themealdb.com' : 'https://www.thecocktaildb.com';
   fireEvent.click(searchInput);
-  fireEvent.change(screen.queryByTestId('search-input'), { target: { value: 'soup' } });
+  fireEvent.change(screen.queryByTestId(SEARCH_INPUT), { target: { value: query } });
   fireEvent.click(screen.queryByTestId(radioButtons[index]));
-  fireEvent.click(screen.queryByTestId('exec-search-btn'));
-  expect(fetch).toHaveBeenCalledWith(`https://www.themealdb.com/api/json/v1/1/search.php?${endURL}=${query}`);
+  fireEvent.click(screen.queryByTestId(BUTTON));
+  expect(fetch)
+    .toHaveBeenLastCalledWith(
+      `${url}/api/json/v1/1/${index === 0 ? 'filter' : 'search'}.php?${endURL}=${query}`,
+    );
 };
 
-describe('Display correct recipe', () => {
+describe('Search correct', () => {
   it('should render correct form when click', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/comidas');
@@ -45,10 +52,27 @@ describe('Display correct recipe', () => {
     renderCorrectForms();
   });
 
-  it('should render correct recipe when search', async () => {
+  it('should render correct recipe when search for name', async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/comidas');
+    search('soup', 1, 's', 'comida');
+    history.push('/bebidas');
+    search('ola', 1, 's', 'bebida');
+  });
 
-    search('soup', 0, 's');
+  it('should render correct recipe when search for ingredient', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    search('chicken', 0, 'i', 'comida');
+    history.push('/bebidas');
+    search('ola', 0, 'i', 'bebida');
+  });
+
+  it('should render correct recipe when search for first letter', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    search('a', 2, 'f', 'comida');
+    history.push('/bebidas');
+    search('a', 2, 'f', 'bebida');
   });
 });
