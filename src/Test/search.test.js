3f3ed1630dfcfe03536f324d0d86581503f1985d
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-11-24 11:49:28 -0300

    Finished search test

--- src/Test/search.test.js
+++ src/Test/search.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './mockFetch';
 import 'jest-canvas-mock';
 import '@testing-library/jest-dom';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 const SEARCH_INPUT = 'search-input';
 const BUTTON = 'exec-search-btn';
+const TOP_BUTTON = 'search-top-btn';
 
 const radioButtons = [
   'ingredient-search-radio', 'name-search-radio', 'first-letter-search-radio',
@@ -20,7 +21,7 @@ const renderCorrectForms = () => {
   expect(screen.queryByTestId(SEARCH_INPUT)).not.toBeInTheDocument();
   expect(screen.queryByTestId(BUTTON)).not.toBeInTheDocument();
 
-  const searchInput = screen.getByTestId('search-top-btn');
+  const searchInput = screen.getByTestId(TOP_BUTTON);
   fireEvent.click(searchInput);
 
   radioButtons.forEach((radioButton) => {
@@ -31,7 +32,7 @@ const renderCorrectForms = () => {
 };
 
 const search = (query, index, endURL, type) => {
-  const searchInput = screen.getByTestId('search-top-btn');
+  const searchInput = screen.getByTestId(TOP_BUTTON);
   const url = type === 'comida' ? 'https://www.themealdb.com' : 'https://www.thecocktaildb.com';
   fireEvent.click(searchInput);
   fireEvent.change(screen.queryByTestId(SEARCH_INPUT), { target: { value: query } });
@@ -74,5 +75,32 @@ describe('Search correct', () => {
     search('a', 2, 'f', 'comida');
     history.push('/bebidas');
     search('a', 2, 'f', 'bebida');
+  });
+});
+
+describe('Alert and redirect if only have 1 recipe', () => {
+  it('should display alert if put more then 1 letter', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/comidas');
+    global.alert = jest.fn();
+    fireEvent.click(screen.getByTestId(TOP_BUTTON));
+    fireEvent.change(screen.queryByTestId(SEARCH_INPUT), { target: { value: 'aa' } });
+    fireEvent.click(screen.queryByTestId(radioButtons[2]));
+    fireEvent.click(screen.queryByTestId(BUTTON));
+    expect(global.alert)
+      .toHaveBeenCalledWith('Sua busca deve conter somente 1 (um) caracter');
+  });
+
+  it('should redirect to recipe details if only have one', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/bebidas');
+    fireEvent.click(screen.getByTestId(TOP_BUTTON));
+    fireEvent.change(
+      screen.queryByTestId(SEARCH_INPUT),
+      { target: { value: 'Aquamarine' } },
+    );
+    fireEvent.click(screen.queryByTestId(radioButtons[1]));
+    fireEvent.click(screen.queryByTestId(BUTTON));
+    await waitFor(() => expect(history.location.pathname).toBe('/bebidas/178319'));
   });
 });
